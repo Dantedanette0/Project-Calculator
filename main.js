d4fe533d00ea8cd52358183ecb3aa7fb1faec370
dantedanette0 <carminedante@yahoo.com>
2025-06-03 19:41:33 -0400

    added math functions and added input boxes to html

---
+++ main.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b == 0){
+    console.log("Cannot divide by 0");
+    return "ERROR";
+  }  
+  return a / b;
+}
+
+
+const a = parseFloat(prompt)
